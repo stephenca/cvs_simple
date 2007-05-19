a1f921b548463a18abf3980b507788e0d098335e
stephenca <>
2007-05-19 13:40:20 +0000

    Added sub. signatures.

--- lib/Cvs/Simple.pm
+++ lib/Cvs/Simple.pm
@@ -3,6 +3,56 @@ package Cvs::Simple;
 use strict;
 use warnings;
 
+sub new {
+
+}
+
+sub callback {
+
+
+}
+
+sub add {
+
+}
+
+sub add_bin {
+
+
+}
+
+sub checkout {
+
+
+}
+
+sub co {
+    goto &checkout;
+}
+
+sub commit {
+
+}
+
+sub ci {
+    goto &commit;
+}
+
+sub diff {
+
+}
+
+sub status {
+
+
+}
+
+sub update {
+
+
+}
+
+
 1;
 __END__
 
