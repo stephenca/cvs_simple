cb3df760cd5a6a509305427b346e7fe52956bcaf
stephenca <>
2008-01-29 15:41:38 +0000

    Update license date.

--- lib/Cvs/Simple.pm
+++ lib/Cvs/Simple.pm
@@ -655,7 +655,7 @@ Stephen Cardie, E<lt>stephenca@ls26.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Stephen Cardie
+Copyright (C) 2007,2008 by Stephen Cardie
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
