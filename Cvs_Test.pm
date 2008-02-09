f38d88540b20ad80da405cb1be1c47b42c6c9498
stephenca <>
2008-02-09 23:40:56 +0000

    Added prototypes to cvs_make() and cvs_clean().

--- Cvs_Test.pm
+++ Cvs_Test.pm
@@ -12,7 +12,7 @@ sub TMPL   () { 'repository'                               }
 sub REP    () { 'cvsdir'                                   }
 sub LOCAL  () { sprintf(':local:%s', catdir(CVSDIR, REP) ) }
 
-sub cvs_make {
+sub cvs_make ($) {
     my($cwd) = shift;
 
     unless((splitdir($cwd))[-1] eq 't') {
@@ -25,17 +25,17 @@ sub cvs_make {
     my(@dir)     = splitdir(rel2abs($cwd));
     my($repldir) = catdir(@dir[0 .. @dir-2], TMPL, 'Add');
 
-chdir($repldir)
-    or die "Can\'t chdir:$!";
-system( CVSBIN, '-d', LOCAL, 'import', '-m', q[""], qw(Add V1 E2) );
+    chdir($repldir)
+       or die "Can\'t chdir:$!";
+    system( CVSBIN, '-d', LOCAL, 'import', '-m', q[""], qw(Add V1 E2) );
 
-chdir($cwd)
-    or die "Can\'t chdir:$!";
+    chdir($cwd)
+        or die "Can\'t chdir:$!";
 
     return;
 }
 
-sub cvs_clean {
+sub cvs_clean () {
 
     my($cwd) = rel2abs(curdir());
     unless((splitdir($cwd))[-1] eq 't') {
